27ca06b2d9066d6a606b2a3ec8ae22a60265ced9
KaengEE <yopuiqwer@naver.com>
2023-11-22 09:30:22 +0900

    파일정리 및 폰트적용

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <nav className="navbar">Navbar content</nav>
+      <main>Main Content</main>
+    </div>
+  );
+}
+
+export default App;
