ad1363f1a1666f0c46d1a4aaffac9338666a9c5f
KaengEE <yopuiqwer@naver.com>
2023-11-22 12:21:27 +0900

    푸터추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Footer } from "./components/Footer/Footer";
 import MovieList from "./components/MovieList/MovieList";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
     <div className="app">
       <Navbar />
       <MovieList />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import "./Footer.css";
+
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer_text">Footer</div>
+      <div className="footer_text">Footer</div>
+      <div className="footer_text">
+        API:
+        <a target="_blank" href="https://www.themoviedb.org/">
+          TMDB
+        </a>
+      </div>
+    </div>
+  );
+};
