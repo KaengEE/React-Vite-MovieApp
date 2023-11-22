ed4d6d760573b8de7f52877b2ec73287e81040ab
KaengEE <yopuiqwer@naver.com>
2023-11-22 11:23:55 +0900

    영화카드 detail 표시

--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -3,7 +3,7 @@ import Star from "../../assets/star.png";
 
 import React from "react";
 
-export default function MovieCard() {
+export default function MovieCard({ movie }) {
   return (
     // 영화 카드
     <a className="movie_card">
@@ -14,19 +14,15 @@ export default function MovieCard() {
       />
 
       <div className="movie_details">
-        <h3 className="movie_details_heading">플래쉬</h3>
+        <h3 className="movie_details_heading">{movie.title}</h3>
         <div className="align_center movie_date_rate">
-          <p>2023-06-14</p>
+          <p>{movie.release_data}</p>
           <p className="align_center">
-            평점
+            {movie.vote_average}
             <img src={Star} alt="rating icon" className="card_emoji" />
           </p>
         </div>
-        <p className="movie_description">
-          빛보다 빠른 스피드, 물체 투과, 전기 방출, 자체 회복, 천재적인 두뇌까지
-          갓벽한 능력을 자랑하지만 존재감은 제로, 저스티스 리그에서 궂은일을
-          도맡아 하는 히어로 ‘플래시’. 어느 날
-        </p>
+        <p className="movie_description">{movie.overview}</p>
       </div>
     </a>
   );
