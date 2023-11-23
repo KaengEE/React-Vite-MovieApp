519d2a5e4ec967985ba6c15cc21c994c660c3b62
KaengEE <yopuiqwer@naver.com>
2023-11-23 12:51:05 +0900

    검색창 제목 추가

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -42,6 +42,7 @@ export const Search = () => {
   //console.log(searchTerm);
   return (
     <div className="search_box">
+      <h3 className="search_title">🔎 영화 검색</h3>
       <input
         className="search"
         value={searchTerm}
