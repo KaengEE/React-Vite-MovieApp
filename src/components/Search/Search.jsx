5b23e808ea3d3da0612eff9499d8ddfe19d71925
KaengEE <yopuiqwer@naver.com>
2023-11-23 12:13:12 +0900

    검색결과 예외처리

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,23 +1,43 @@
 import MovieCard from "../MovieList/MovieCard";
 import "./Search.css";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Search = () => {
+  //const saveTerm = localStorage.getItem("searchTerm"); //로컬에 저장된 검색어 불러오기
   const [searchTerm, setSearchTerm] = useState(""); // 검색어
   const [searchResults, setSearchResults] = useState([]); // 검색 결과
+  const [noResults, setNoResults] = useState(false); // 검색 결과가 없는지 여부
+  const [search, setSearch] = useState(""); //검색시 검색어 저장
 
   async function SearchMovie() {
-    console.log(searchTerm);
+    //console.log(searchTerm);
+    setSearch(searchTerm);
     const response = await fetch(
       `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&api_key=${
         import.meta.env.VITE_MOVIE_API
       }&language=ko`
     );
     const data = await response.json();
-    // 검색 결과 저장
-    setSearchResults(data.results);
+
+    // 검색 결과가 없으면 return
+    if (data.results.length === 0) {
+      setNoResults(true);
+      //초기화
+      setSearchResults([]);
+      setSearchTerm("");
+    } else {
+      // 검색 결과 저장
+      setSearchResults(data.results);
+      setSearchTerm("");
+      setNoResults(false); // 검색 결과가 있음}
+    }
   }
+
+  //로컬에 저장
+  useEffect(() => {
+    localStorage.setItem("searchTerm", JSON.stringify(searchTerm));
+  }, [searchResults]);
 
   //console.log(searchTerm);
   return (
@@ -31,8 +51,15 @@ export const Search = () => {
       <button onClick={SearchMovie}>검색</button>
 
       <div>
-        <p className="search_term">'{searchTerm}'로 검색한 결과입니다.</p>
+        {noResults && searchResults < 1 ? (
+          <p className="search_term">검색 결과가 없습니다.</p>
+        ) : null}
+
+        {searchResults.length > 1 ? (
+          <p className="search_result">'{search}'으로 검색한 결과입니다.</p>
+        ) : null}
       </div>
+
       {/* 검색된 영화 카드들 */}
       <div className="movie_cards">
         {searchResults.map((movie) => (
