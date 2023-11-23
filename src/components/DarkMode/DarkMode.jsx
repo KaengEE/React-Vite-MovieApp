1c52ddbebd77e3e792d2afdbf898083135a69ce1
KaengEE <yopuiqwer@naver.com>
2023-11-23 09:12:13 +0900

    라이트모드로 시작

--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./DarkMode.css";
 import Sun from "../../assets/Sun.svg?react";
@@ -9,10 +9,15 @@ const DarkMode = () => {
     document.querySelector("body").setAttribute("data-theme", "dark");
   const setLightTheme = () =>
     document.querySelector("body").setAttribute("data-theme", "light");
-  const toggleTheme = (e) =>
+  // 토글 누르면 작동하는 함수
+  const toggleTheme = (e) => {
     e.target.checked ? setDarkTheme() : setLightTheme();
+  };
 
-  setDarkTheme(); //다크테마로 시작
+  //시작시 라이트 테마
+  useEffect(() => {
+    document.querySelector("body").setAttribute("data-theme", "light");
+  }, []);
 
   return (
     <div className="dark_mode">
